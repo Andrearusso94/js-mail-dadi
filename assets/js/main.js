ef295a5a3b8ff85b14f6813812e2ca5dfa777a59
Andrearusso94 <andrea.r.4845@gmail.com>
2022-10-11 14:27:54 +0200

    add user email

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,8 @@
 //Mail
 //Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
 
-const listEmail = ["andrea.r.4845@gmail.com", "andrea.r.94.varie@gmail.com", "giordanategazio@gmail.com"]
+const listEmail = ["andrea.r.4845@gmail.com", "andrea.r.94.varie@gmail.com", "giordanategazio@gmail.com"];
+
+//Chiedi all’utente la sua email
+const myEmail = prompt("dimmi la tua email");
+console.log(myEmail)
