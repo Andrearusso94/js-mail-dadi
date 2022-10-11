e775e1b8df0ad2d85926848208182dd3c3af2098
Andrearusso94 <andrea.r.4845@gmail.com>
2022-10-11 14:25:48 +0200

    add array email

---
+++ assets/js/main.js
@@ -0,0 +1,4 @@
+//Mail
+//Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
+
+const listEmail = ["andrea.r.4845@gmail.com", "andrea.r.94.varie@gmail.com", "giordanategazio@gmail.com"]
