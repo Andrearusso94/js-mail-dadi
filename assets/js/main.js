749300294f25170581a48ee2e5fa14d208b781e1
Andrearusso94 <andrea.r.4845@gmail.com>
2022-10-12 10:54:07 +0200

    add correct random number for mynumber

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 //Mail
 //Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
 
-const listEmail = ["andrea.r.4845@gmail.com", "andrea.r.94.varie@gmail.com", "giordanategazio@gmail.com"];
+/*const listEmail = ["andrea.r.4845@gmail.com", "andrea.r.94.varie@gmail.com", "giordanategazio@gmail.com"];
 
 //Chiedi all’utente la sua email
 const myEmail = prompt("dimmi la tua email");
@@ -17,15 +17,21 @@ if  ( myEmail === element ){
    
 } 
 
-} console.log(isValid) 
+} console.log(isValid) */
 
-
+// another solution const isValid senza false ma vuota
+//altro ciclo if 
+/* if(isValid){
+ console.log('Welcome in the site')
+} else {
+  console.log('this site is private')
+}*/
 
 //Gioco dei dadi
 
 //Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. 
 
-/*const myNumber = Number(prompt ('Scegli il tuo numero tra 1 e 6'));
+const myNumber = Math.floor(Math.random() *6)+1;
 const pcNumber = Math.floor(Math.random() *6)+1;
 
 
@@ -41,4 +47,4 @@ if (myNumber > pcNumber){
     console.log("hai perso", pcNumber)
 } else  {
     console.log("avete pareggiato scegli un altro numero")
-}*/
+}
